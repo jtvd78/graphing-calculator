c5cd880f71976aa65817d01ed7c9d6b1ac9ffb11
Justin <Justin@129.2.195.221>
2016-02-09 18:04:35 -0500

    a

--- Graphing Calculator/src/com/justin/function/Function.java
+++ Graphing Calculator/src/com/justin/function/Function.java
@@ -10,15 +10,17 @@ public class Function {
 	Color col;
 	Term mainTerm;
 	
+	public Function(String functionString, Color c){
+		col = c;
+		mainTerm = TermReader.readTerm(functionString);
+	}
+	
 	public Function(Term t){
 		col = Color.RED;
 		mainTerm = t;
 	}
 	
-	public Function(String functionString, Color c){
-		col = c;
-		mainTerm = TermReader.readTerm(functionString);
-	}	
+		
 	
 	public double getY(double x){
 		mainTerm.setVariable("x", x);
@@ -27,7 +29,9 @@ public class Function {
 
 	public String toString(){
 		return mainTerm.toString();
-	}
+	}	
+	
+	
 	
 	public Color getColor(){
 		return col;
